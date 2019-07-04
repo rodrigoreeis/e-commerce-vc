e8cfb91036c59aecca106f9d1d19bc6a0320ef5a
Rodrigo Reis <roreis99@gmail.com>
2019-07-03 21:52:20 -0300

    commit home

--- gulpfile.js
+++ gulpfile.js
@@ -5,75 +5,74 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const del = require('del');
 const browserify = require('gulp-browserify');
-const babelify = require('babelify');
 const pug = require('gulp-pug');
 
 const paths = {
-  styles: {
-    src: 'src/assets/scss/common/*.scss',
-    dest: './dist/assets/css',
-    srcWatch: 'src/assets/scss/**/*.scss',
-  },
-  scripts: {
-    src: 'src/assets/js/common/*.js',
-    dest: './dist/assets/js',
-    srcWatch: 'src/assets/**/*.js',
-  },
-  htmls: {
-    src: 'src/views/common/**/*.pug',
-    dest: './dist/views/html',
-  }
+	styles: {
+		src: 'src/assets/scss/common/*.scss',
+		dest: './dist/assets/css',
+		srcWatch: 'src/assets/scss/**/*.scss',
+	},
+	scripts: {
+		src: 'src/assets/js/common/*.js',
+		dest: './dist/assets/js',
+		srcWatch: 'src/assets/**/*.js',
+	},
+	htmls: {
+		src: 'src/views/common/**/*.pug',
+		dest: './dist/views/html',
+	}
 };
 
 const clean = () => {
-  return del(['dist']);
-}
+	return del(['dist']);
+};
 
 const htmls = () => {
-  return gulp.src([
-    paths.htmls.src,
-    '!src/views/common/_layouts/*.pug',
-    '!src/views/common/_partials/**/*.pug',
-  ])
-    .pipe(pug({
-      pretty: true,
-    }))
-    .pipe(gulp.dest(paths.htmls.dest));
-}
+	return gulp.src([
+		paths.htmls.src,
+		'!src/views/common/_layouts/*.pug',
+		'!src/views/common/_partials/**/*.pug',
+	])
+		.pipe(pug({
+			pretty: true,
+		}))
+		.pipe(gulp.dest(paths.htmls.dest));
+};
 const styles = () => {
-  return gulp.src(paths.styles.src)
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer({
-      browsers: ['last 10 versions'],
-      cascade: false,
-    }))
-    .pipe(gulp.dest(paths.styles.dest));
-}
+	return gulp.src(paths.styles.src)
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(autoprefixer({
+			browsers: ['last 10 versions'],
+			cascade: false,
+		}))
+		.pipe(gulp.dest(paths.styles.dest));
+};
 
 const scripts = () => {
-  return gulp.src(paths.scripts.src)
-    .pipe(babel({
-      presets: ["@babel/polyfill"]
-    }))
-    .pipe(browserify({
-      transform: ['babelify'],
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.scripts.dest));
-}
+	return gulp.src(paths.scripts.src)
+		.pipe(babel({
+			presets: ['@babel/polyfill']
+		}))
+		.pipe(browserify({
+			transform: ['babelify'],
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.scripts.dest));
+};
 
 const pluginsJs = () => {
-  return gulp.src(
-    ['node_modules/vtex-lazyload/dist/*.js']
-  )
-    .pipe(gulp.dest(paths.scripts.dest));
-}
+	return gulp.src(
+		['node_modules/vtex-lazyload/dist/*.js']
+	)
+		.pipe(gulp.dest(paths.scripts.dest));
+};
 
 const watch = () => {
-  gulp.watch(paths.styles.srcWatch, styles);
-  gulp.watch(paths.scripts.srcWatch, scripts);
-  gulp.watch('src/views/common/**/*.pug', htmls);
-}
+	gulp.watch(paths.styles.srcWatch, styles);
+	gulp.watch(paths.scripts.srcWatch, scripts);
+	gulp.watch('src/views/common/**/*.pug', htmls);
+};
 
 
 const build = gulp.series(clean, gulp.parallel(styles, scripts, htmls, pluginsJs));
